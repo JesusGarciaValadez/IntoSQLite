29cbe6ecffb7ee5c8043bcbe4af468efe031afcb
Jesús Antonio García Valadez <zerovamp@hotmail.com>
2016-09-06 17:38:31 -0500

    Import FMDB libraries in bridge header. Adding labels and buttons for sqlite excercise.

--- IntoSQLite/ViewController.swift
+++ IntoSQLite/ViewController.swift
@@ -10,6 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var saveCarModel: UITextField!
+  @IBOutlet weak var saveCarPrice: UITextField!
+  @IBOutlet weak var searchCarModel: UITextField!
+
+  @IBAction func saveCarButton(_ sender: AnyObject) {
+
+    if self.saveCarModel.hasText && self.saveCarPrice.hasText {
+      if let carModel = self.saveCarModel.text , let carPrice = Int ( self.saveCarPrice.text! ) {
+        self.saveCar( model : carModel, price : carPrice )
+      }
+    }
+  }
+
+  @IBAction func searchCarButton(_ sender: AnyObject) {
+
+    if self.searchCarModel.hasText, let carModel = self.searchCarModel.text {
+      self.searchCar( model: carModel )
+    }
+  }
+
+  func saveCar( model : String, price : Int ) {
+    print( "Auto modelo: \(model) guardado con el precio: \(price))" )
+  }
+
+  func searchCar( model : String ) {
+    print( "Resultado: \(model)" )
+  }
+
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +47,6 @@ class ViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
 
 }
 
